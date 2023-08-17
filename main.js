c462fa1ab9c7867d985aee5bccf2505ad3401359
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-08-17 17:40:21 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/two-sum
+
+// **
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+var twoSum = function(nums, target) {
+    let output = [];
+    for(let i = 0; i < nums.length-1; i++) { //On
+        for(let j = i+1; j < nums.length; j++ ){ //nLogn
+            if(nums[i] + nums[j] === target){
+            output.push(i)
+            output.push(j)
+            }
+        }
+    }
+    return output
+};
+
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j <nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i, j]
+            } 
+        }
+    }
+}
